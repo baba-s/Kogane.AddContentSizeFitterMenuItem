f9b0a728c56cc93d7696f2c1687dbb3f3c498f38
baba-s <shotababa1206@gmail.com>
2022-08-26 21:17:02 +0900

    first commit

---
+++ Editor/AddContentSizeFitterMenuItem.cs
@@ -0,0 +1,33 @@
+﻿using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Kogane.Internal
+{
+    internal static class AddContentSizeFitterMenuItem
+    {
+        [MenuItem( "CONTEXT/LayoutGroup/Add Content Size Fitter" )]
+        [MenuItem( "CONTEXT/Text/Add Content Size Fitter" )]
+        [MenuItem( "CONTEXT/TMP_Text/Add Content Size Fitter" )]
+        private static void AddContentSizeFitter( MenuCommand menuCommand )
+        {
+            var component         = ( Component )menuCommand.context;
+            var gameObject        = component.gameObject;
+            var contentSizeFitter = Undo.AddComponent<ContentSizeFitter>( gameObject );
+            var mode              = ContentSizeFitter.FitMode.PreferredSize;
+
+            contentSizeFitter.horizontalFit = mode;
+            contentSizeFitter.verticalFit   = mode;
+        }
+
+        [MenuItem( "CONTEXT/LayoutGroup/Add Content Size Fitter", true )]
+        [MenuItem( "CONTEXT/Text/Add Content Size Fitter", true )]
+        [MenuItem( "CONTEXT/TMP_Text/Add Content Size Fitter", true )]
+        private static bool CanAddContentSizeFitter( MenuCommand menuCommand )
+        {
+            var component = ( Component )menuCommand.context;
+
+            return !component.TryGetComponent<ContentSizeFitter>( out _ );
+        }
+    }
+}
